88454a729c017c5a40bdfc9b36f389f687e24ad6
matthewwaite <matthewbwaite@gmail.com>
2014-05-08 11:33:50 -0700

    video added

--- js/main.js
+++ js/main.js
@@ -199,8 +199,9 @@ $(document).ready(function () {
             left: (window_width) + "px"
         });
         $('.story-content').css({
-        	height: (window_height * .7) + "px"
+        	height: (window_height * .7) + "px",
         });
+        
     }
 
     viewport_height();
@@ -248,39 +249,13 @@ jQuery(document).ready(function ($) {
     isFitWidth: true
   });
   
-  var arrGreen = new Array($('.i1'),$('.i3'),$('.i6'),$('.i7'));
-  var arrRed = new Array($('.i2'),$('.i4'),$('.i5'),$('.i8'), $('.i9'),$('.i10'),$('.i11'),$('.i12'), $('.i13'),$('.i14'),$('.i15'));
   
-  $('input').click(function () {
-    $('.item').removeClass('expand');
-    
-    if ($('.green').is(':checked')) {
-      $container.masonry('hide', arrRed);
-      $(arrGreen).each(function (index, element) {
-        element.show();
-      });
-      $container.masonry();
-    } else if ($('.red').is(':checked')) {
-      $container.masonry('hide', arrGreen);
-      $(arrRed).each(function (index, element) {
-        element.show();
-      });
-      $container.masonry();
-    } else if ($('.all').is(':checked')) {
-      $(arrGreen).each(function (index, element) {
-        element.show();
-      });
-      $(arrRed).each(function (index, element) {
-        element.show();
-      });
-      $container.masonry();
-    }
-  });
   
-  $('.item').click(function () {
-    $(this).toggleClass('expand');
-  });
-  
+});
+
+$(document).load(function () {
+    var none = $('.fullscreen')
+    document.find('.fullscreen').style.display = "none";
 });
 
 // left: 37, up: 38, right: 39, down: 40,
