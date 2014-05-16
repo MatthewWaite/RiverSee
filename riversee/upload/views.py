02ebb73f489bfc5e16664d95c7d40287273ff667
Julian Threatt <porfirio84@gmail.com>
2014-05-15 17:05:08 -0700

    vimeo shows up dynamically

--- riversee/upload/views.py
+++ riversee/upload/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 from upload.models import Upload
+import requests
 
 
 # Create your views here.
@@ -8,3 +9,9 @@ class UploadView(CreateView):
     model = Upload
     success_url = '/'
     template_name = 'upload/index.html'
+
+    def get_context_data(self, **kwargs):
+        r = requests.get('http://vimeo.com/api/v2/channel/457116/videos.json')
+        if r.status_code == 200:
+            kwargs['videos'] = r.json()
+        return super(UploadView, self).get_context_data(**kwargs)
